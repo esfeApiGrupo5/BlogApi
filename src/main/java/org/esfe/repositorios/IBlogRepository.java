b5a5983d7b4c050cfe199c025dbd751e0d73e87b
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-09-11 10:02:43 -0600

    ECM-20 <se agrego para obtener el id de usuario en el repository>

--- src/main/java/org/esfe/repositorios/IBlogRepository.java
+++ src/main/java/org/esfe/repositorios/IBlogRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface IBlogRepository  extends JpaRepository<Blog, Integer> {
+    List<Blog> findByUsuarioId(Integer usuarioId);
     List<Blog> findByTituloContainingIgnoreCaseOrDescripcionContainingIgnoreCase(String titulo, String descripcion);
 }
 //se añadio comentario para poder subir el commit y relacionarlo con jira ya que sino no deja vincularlo.
