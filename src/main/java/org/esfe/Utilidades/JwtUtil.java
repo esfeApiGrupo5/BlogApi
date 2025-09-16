56254dffc9304bc119adc978954d9a883c2a5d1b
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-09-16 11:39:48 -0600

    se creo el atenticar pero no funciona el jwtUtil

---
+++ src/main/java/org/esfe/Utilidades/JwtUtil.java
@@ -0,0 +1,56 @@
+package org.esfe.Utilidades;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Component
+public class JwtUtil {
+
+    // ¡IMPORTANTE! Debe ser la misma clave secreta que en la API de usuarios.
+    public static final String SECRET_KEY = "${security.jwt.secret-key}";
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public Boolean validateToken(String token) {
+        try {
+            return !isTokenExpired(token);
+        } catch (Exception e) {
+            // Maneja cualquier excepción de validación (firma incorrecta, token inválido, etc.)
+            return false;
+        }
+    }
+}
